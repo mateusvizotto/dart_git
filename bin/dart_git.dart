e479b3735f32242426426daf98c4b704b868cfec
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2022-01-20 12:54:09 -0300

    Comentários deletados

--- bin/dart_git.dart
+++ bin/dart_git.dart
@@ -3,6 +3,3 @@ import 'package:dart_git/dart_git.dart' as dart_git;
 void main(List<String> arguments) {
   print('Hello world: ${dart_git.calculate()}!');
 }
-
-//! Alteração no arq
-//! Outra alteração no arq
